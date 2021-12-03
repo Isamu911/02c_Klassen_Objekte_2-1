cdce6472d9ec7417d86a533a73b63bc8eca63645
Isamu911 <mwasielewski@gmx.de>
2021-12-03 12:48:01 +0100

    get age

--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -7,6 +7,8 @@ public class Cat {
     private int age;
     private boolean isFemale;
 
+    private int counter;
+
     public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
@@ -38,7 +40,7 @@ public class Cat {
     }
 
 
-    public String checkCompliance() {
+    private String checkCompliance() {
         if (isFemale) {
            return  checkEscalationLevel(); 
         } else {
@@ -47,9 +49,9 @@ public class Cat {
 
     }
 
-  String checkEscalationLevel(){
+   private String checkEscalationLevel(){
 
-        int counter = 1;
+        counter++;
 
         switch (counter) {
             case 1:
